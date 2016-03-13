98a175bce341bd8b64f0f3c9939f42098c13a08d
Magdalena Grzmiel <grzmiel@outlook.com>
2016-03-13 16:47:21 -0700

    Adding the code toread the data to each file

--- plot2.R
+++ plot2.R
@@ -2,7 +2,30 @@
 # Magdalena Grzmiel
 # Assignment #1
 
-# Use the same data frame which was readed to R in plot1.R file
+# Read the data
+# set the working directory
+setwd("/Users/magda/Desktop/Coursera/expoloratory_data_analysis/ExData_Plotting1")
+
+# read the data 
+dat <-read.table("/Users/magda/Desktop/Coursera/expoloratory_data_analysis/household_power_consumption.txt", header=TRUE,
+                 sep=";")
+
+# read in date/time info in format d/m/y h:m:s
+dat$timestamp<-paste(dat$Date, dat$Time)
+dat$timestamp<-as.character(dat$timestamp)
+dat$time_obj<-strptime(dat$timestamp, "%d/%m/%Y %H:%M:%S")
+
+# convert Date to Date object
+dat$Date<-as.Date(dat$Date, "%d/%m/%Y")
+
+
+#subset the data to only include data from Feb 1st and 2nd
+feb.data <-dat[dat$Date>=as.Date("2007-02-01") & dat$Date<=as.Date("2007-02-02"),]
+
+# convert all numeric column to numeric format
+for (i in 3:9){
+  feb.data[,i]<-as.numeric(as.character(feb.data[,i]))
+}
 
 # Code to generate Plot #2
 png("plot2.png",width=480,height=480)
--- plot4.R
+++ plot4.R
@@ -2,9 +2,33 @@
 # Magdalena Grzmiel
 # Assignment #1
 
-# Use the same data frame which was readed to R in plot1.R file
+# Read the data
+# set the working directory
+setwd("/Users/magda/Desktop/Coursera/expoloratory_data_analysis/ExData_Plotting1")
 
-# Plot #4
+# read the data 
+dat <-read.table("/Users/magda/Desktop/Coursera/expoloratory_data_analysis/household_power_consumption.txt", header=TRUE,
+                 sep=";")
+
+# read in date/time info in format d/m/y h:m:s
+dat$timestamp<-paste(dat$Date, dat$Time)
+dat$timestamp<-as.character(dat$timestamp)
+dat$time_obj<-strptime(dat$timestamp, "%d/%m/%Y %H:%M:%S")
+
+# convert Date to Date object
+dat$Date<-as.Date(dat$Date, "%d/%m/%Y")
+
+
+#subset the data to only include data from Feb 1st and 2nd
+feb.data <-dat[dat$Date>=as.Date("2007-02-01") & dat$Date<=as.Date("2007-02-02"),]
+
+# convert all numeric column to numeric format
+for (i in 3:9){
+  feb.data[,i]<-as.numeric(as.character(feb.data[,i]))
+}
+
+
+# Code to generate the Plot #4
 png("plot4.png",width=480,height=480)
 par(mfrow=c(2,2))
 with(feb.data, {
